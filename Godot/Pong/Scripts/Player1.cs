085baac357d546c7d544d7533d1e5628033d9229
Celine Ye <yeceline26@gmail.com>
2025-10-02 19:30:27 +0200

    add beginning of project

---
+++ Godot/Pong/Scripts/Player1.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+using GameCore;
+using TestSolution;
+
+public partial class Player1 : Area2D
+{
+	//<ProjectReference Include="..\..\GameCore\Scripts\JSP.cstruc" /> 
+	private Class1 a;
+	private Class2 b;
+	private TestClass c;
+}
