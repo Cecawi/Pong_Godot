ae294dd20d7b8ea50621ba6d72f925e9a64fbd81
Celine Ye <yeceline26@gmail.com>
2025-10-10 11:02:06 +0200

    Update PlayerIntention.cs

--- GameCore/GameCore/GameCore/PlayerIntention.cs
+++ GameCore/GameCore/GameCore/PlayerIntention.cs
@@ -4,22 +4,22 @@ public class PlayerIntention
 {
     //attribut
     public int Move {get;}// -1 = haut, 0 = neutre, 1 = bas : intention du joueur (lors de l'update)
+    
+    
 
     public PlayerIntention(int m)
     {
-        //à remplir
+        if (m < -1 || m > 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(m), "La valeur doit être -1, 0 ou 1");
+        }
+        Move = m;
     }
+    
     /*
-    // Constructeur : on passe directement l'intention du joueur
-    public PlayerIntention(int move)
-    {
-        if (move < -1 || move > 1)
-            throw new ArgumentOutOfRangeException(nameof(move), "Move doit être -1, 0 ou 1");
-        
-        Move = move;
-    }
-
-    // Méthode statique pour créer l'intention à partir d'une touche Godot
+     normalement, on en n'a pas besoin
+     A ENLEVER
+    // Méthode statique pour créer l'intention à partir d'une touche_clavier Godot
     public static PlayerIntention FromInput(bool upPressed, bool downPressed)
     {
         int move = 0;
@@ -27,6 +27,5 @@ public class PlayerIntention
         else if (downPressed) move = 1;
 
         return new PlayerIntention(move);
-    }
-     */
+    }*/
 }
