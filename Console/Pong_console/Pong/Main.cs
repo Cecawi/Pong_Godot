e9331da7a53e05070a509833656540777821afa5
Celine Ye <yeceline26@gmail.com>
2025-10-29 01:07:13 +0100

    First frame
    ajout de l'affichage de la balle et des paddels

--- Console/Pong_console/Pong/Main.cs
+++ Console/Pong_console/Pong/Main.cs
@@ -7,6 +7,10 @@ public class MainConsole
 {
     private static GameState _gameState;
     
+    private float ballX, ballY, paddle1X, paddle1Y, paddle2X, paddle2Y;
+
+    private int gx, gy, p1gx, p1gy, p2gx, p2gy;
+    
     //monde dans GameCore :
     //en haut à gauche : (-0.5, -0.5)
     //en bas à droite : (+0.5, +0.5)
@@ -35,7 +39,22 @@ public class MainConsole
                 grid[i, j] = ' ';
             }
         }
-
+        
+        //récupère les positions depuis GameCore
+        var (ballX, ballY) = _gameState.GetBallPosition();
+        var (paddle1X, paddle1Y) = _gameState.GetPaddle1Position();
+        var (paddle2X, paddle2Y) = _gameState.GetPaddle2Position();
+        
+        //convertit les positions dans monde (GameCore) à la case correspondante dans le tableau (qu'on affiche)
+        var (gx, gy) = ToGrid(ballX, ballY);
+        var (p1gx, p1gy) = ToGrid(paddle1X, paddle1Y);
+        var (p2gx, p2gy) = ToGrid(paddle2X, paddle2Y);
+        
+        //place les objets dans le tableau
+        if (gy >= 0 && gy <= Height && gx >= 0 && gx <= Width) grid[gy, gx] = 'o';//balle
+        if (p1gy >= 0 && p1gy <= Height && p1gx >= 0 && p1gx <= Width) grid[p1gy, p1gx] = '1';//paddle gauche
+        if (p2gy >= 0 && p2gy <= Height && p2gx >= 0 && p2gx <= Width) grid[p2gy, p2gx] = '1';//paddle droit
+        
         //trace le cadre
         for (int i = 0 ; i <= Width ; i++)
         {
@@ -49,7 +68,7 @@ public class MainConsole
         }
         
         //affichage dans la console
-        //////////////Console.SetCursorPosition(0, 0);
+///////////////////////////////////Console.SetCursorPosition(0, 0);
         for(int i = 0 ; i <= Height ; i++)
         {
             for(int j = 0 ; j <= Width; j++)
@@ -58,5 +77,14 @@ public class MainConsole
             }
             Console.WriteLine();
         }
+    }
+    
+    private static (int x, int y) ToGrid(float normX, float normY)//même idée que ToPixels dans le Main.cs dans Godot
+    {
+        //(-0.5, -0.5) → (0,0)
+        //(+0.5, +0.5) → (Width, Height)
+        int x = (int)((normX + 0.5f) * Width);
+        int y = (int)((normY + 0.5f) * Height);
+        return (x, y);
     }
 }
